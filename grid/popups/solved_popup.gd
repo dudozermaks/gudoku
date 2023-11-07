c59b03bc9d1f95a77cfbf79166218cb7e92c3e41
maks <maks.razrushitel08@gmail.com>
2023-11-07 18:37:34 +0300

    add separate folder for grid popups

--- grid/popups/solved_popup.gd
+++ grid/popups/solved_popup.gd
@@ -1,9 +1,14 @@
 extends Popup
+class_name SolvedPopup
 
 @onready var text := $Control/MarginContainer/VBoxContainer/Text
+@onready var initial_text : String = text.text
 
 func format_text(args : Array):
 	text.text = text.text % args
 
 func _on_back_button_pressed():
-	visible = false
+	hide()
+
+func _on_popup_hide():
+	text.text = initial_text
